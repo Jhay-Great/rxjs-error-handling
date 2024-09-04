ae58fccc50177b5e9aa88d765e9f502703ecc657
Jhay-Great <alemonai.jenef@gmail.com>
2024-09-04 11:27:08 +0000

    initial commit and adds initial folders and files

---
+++ src/app/components/http-request/http-request.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-http-request',
+  standalone: true,
+  imports: [],
+  templateUrl: './http-request.component.html',
+  styleUrl: './http-request.component.css'
+})
+export class HttpRequestComponent {
+
+}
