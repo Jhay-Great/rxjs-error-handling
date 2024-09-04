d4f3cb1cc8b3aff182424c8489dd9959acdf4ced
Jhay-Great <alemonai.jenef@gmail.com>
2024-09-04 12:04:04 +0000

    adds logic for service and component

--- src/app/components/http-request/http-request.component.ts
+++ src/app/components/http-request/http-request.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpRequestService } from '../../services/http-request.service';
 
 @Component({
   selector: 'app-http-request',
@@ -8,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrl: './http-request.component.css'
 })
 export class HttpRequestComponent {
+  loading!: boolean;
+  result!: string;
+  error!: null;
+  // httpService: any;
+
+  constructor (
+    private httpService: HttpRequestService,
+  ) {};
+
+
+  makeRequest() {
+    this.loading = true;
+    // this.result = 'null';
+    // this.error = null;
+
+    this.httpService.httpRequest('Sample Data').subscribe({
+      next: (data) => {
+        this.result = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.error = err.message;
+        this.loading = false;
+      }
+    });
+  }
 
 }
--- src/app/services/http-request.service.ts
+++ src/app/services/http-request.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { delay, map, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpRequestService {
+  private successRate = 0.7;
 
   constructor() { }
+
+  httpRequest<T>(data: T): Observable<T> {
+    return of(null).pipe(
+      delay(1000), // Simulate 1 second of network latency
+      map(() => {
+        if (Math.random() < this.successRate) {
+          return data;
+        } else {
+          throw new Error('Simulated HTTP request failed');
+        }
+      })
+    );
+  }
+  
 }
